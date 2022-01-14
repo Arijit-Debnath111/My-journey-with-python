2ae509306c67d7925ab6e5dc5b39975efc18ad9f
infinite_som <85605330+Arijit-Debnath111@users.noreply.github.com>
2022-01-14 20:59:30 +0530

    Add files via upload

---
+++ Zeros and Ones.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import numpy
+N = tuple(map(int, input().split()))
+print(numpy.zeros(N, int))
+print(numpy.ones(N, int))
+
+
+# In[ ]:
+
+
+
+
