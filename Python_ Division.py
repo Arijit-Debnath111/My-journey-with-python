cd5a9cf0a4c5bd43ff1c7f97f6707b642a4cac17
infinite_som <85605330+Arijit-Debnath111@users.noreply.github.com>
2022-01-14 12:58:49 +0530

    Add files via upload

---
+++ Python_ Division.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+from __future__ import division
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+
+print(int(a//b))
+print(float(a/b))
+
+
+# In[ ]:
+
+
+
+
