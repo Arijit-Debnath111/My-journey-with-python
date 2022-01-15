63f6572445d34d660cb636823144bfd7d3b32f2e
infinite_som <85605330+Arijit-Debnath111@users.noreply.github.com>
2022-01-15 15:30:29 +0530

    Add files via upload

---
+++ Tuples.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+if __name__ == '__main__':
+    n = int(input())
+    integer_list = map(int, raw_input().split())
+    t=tuple(integer_list)
+    print(hash(t))
+
+
+# In[ ]:
+
+
+
+
